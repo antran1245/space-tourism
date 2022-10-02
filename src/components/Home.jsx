c6a669e047e6d85519a93a1c86c097c5a87168cd
antran1245 <antran1245@gmail.com>
2022-10-02 12:27:39 -0700

    setting home image to flex on screen size

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import  { Container } from 'react-bootstrap'
+import logo from '../assets/shared/logo.svg'
+import '../sass/home.scss'
+
+export default function Home() {
+    return(
+        <Container fluid id="home">
+            hi
+        </Container>
+    );
+}
