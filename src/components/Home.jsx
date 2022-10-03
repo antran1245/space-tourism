c60ca65be74d21f8c49b865473eca67bd18bbb5d
antran1245 <antran1245@gmail.com>
2022-10-03 14:19:41 -0700

    add selected and hover onto nav

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import  { Container } from 'react-bootstrap'
-import logo from '../assets/shared/logo.svg'
 import '../sass/home.scss'
 
 export default function Home() {
     return(
         <Container fluid id="home">
+            hi
         </Container>
     );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ import '../sass/nav.scss'
 
 export default function Navigation() {
     const [show, setShow] = useState(false)
-
+    const [selected, setSelected] = useState(0)
     return(
         <Navbar collapseOnSelect expand="md" id='navigation'>
             <Container fluid className='d-flex align-items-space pt-4 pt-md-0'>
@@ -18,10 +18,10 @@ export default function Navigation() {
                 <Navbar.Collapse id="responsive-nav" className={`navLink justify-content-end mt-2 mt-md-0 ${show? 'show open': ''} ${show? '': 'show closed'}`}>
                     <div id='line-bar' className='d-none d-lg-inline'/>
                     <Nav className='d-flex justify-content-around align-items-center'>
-                        <Nav.Link href=""><span>00</span> HOME</Nav.Link>
-                        <Nav.Link href=""><span>01</span> DESTINATION</Nav.Link>
-                        <Nav.Link href=""><span>02</span> CREW</Nav.Link>
-                        <Nav.Link href=""><span>03</span> TECHNOLOGY</Nav.Link>
+                        <Nav.Link href="" className={`${selected === 0 ? 'active' : ''}`} onClick={() => setSelected(0)}><span>00</span> HOME</Nav.Link>
+                        <Nav.Link href="" className={`${selected === 1 ? 'active' : ''}`} onClick={() => setSelected(1)}><span>01</span> DESTINATION</Nav.Link>
+                        <Nav.Link href="" className={`${selected === 2 ? 'active' : ''}`} onClick={() => setSelected(2)}><span>02</span> CREW</Nav.Link>
+                        <Nav.Link href="" className={`${selected === 3 ? 'active' : ''}`} onClick={() => setSelected(3)}><span>03</span> TECHNOLOGY</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
