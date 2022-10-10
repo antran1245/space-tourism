79664dbdfaa9870b474edcfdc7e7a0c5a637440b
antran1245 <antran1245@gmail.com>
2022-10-09 23:13:00 -0700

    dynamically change destination

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -5,6 +5,7 @@ import '../sass/destination.scss'
 
 export default function Destination() {
     const [destinations, setDestination] = useState(data.destinations)
+    const [location, setLocation] = useState(0)
     // parse the image name with a / at the beginning to deal with webpack error
     const [images, setImages] = useState(data.destinations.map((item) => item.images.png.slice(item.images.png.lastIndexOf('/'))))
     return(
@@ -17,61 +18,63 @@ export default function Destination() {
                 </Row>
 
                 {/* Change in location based on selected destination */}
-                <Row className="d-flex justify-content-center mt-2 mt-lg-5">
-                    <Col xs={12} lg={{offset: 1, span: 6}} className="d-flex justify-content-center">
-                        <img src={require("../assets/destination" + images[0])} alt={destinations[0].name}/>
-                    </Col>
-                    <Col className="content-info">
-                        <Row>
-                            <Col xs={12} lg={6} className="d-flex mt-4 mt-xxl-0">
-                                <div className="tabs d-flex">
-                                    <p>MOON</p>
-                                    <p>MARS</p>
-                                    <p>EUROPA</p>
-                                    <p>TITAN</p>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col xs={12} lg={12} className="mt-3">
-                                <h1>{destinations[0].name.toUpperCase()}</h1>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col xs={9} md={8} lg={8} xxl={6}>
-                                <p className="description">{destinations[0].description}</p>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col xs={9} md={8} lg={9} xxl={6} className="distance-travel d-flex align-items-end mt-4 m-xxl-0">
-                                <Col xs={12} sm={6} lg={7} xl={8}>
-                                    <Row>
-                                        <Col>
-                                            <p className="distance-text">AVG. DISTANCE</p>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <p className="distance">{destinations[0].distance.toUpperCase()}</p>
-                                        </Col>
-                                    </Row>
+                {destinations.map((item, index) => {
+                    return <Row className={`${index === location? 'd-flex' : 'd-none'} justify-content-center mt-2 mt-lg-5`} key={index}>
+                        <Col xs={12} lg={{offset: 1, span: 6}} className="d-flex justify-content-center">
+                            <img src={require("../assets/destination" + images[index])} alt={item.name}/>
+                        </Col>
+                        <Col className="content-info">
+                            <Row>
+                                <Col xs={12} lg={6} className="d-flex mt-4 mt-xxl-0">
+                                    <div className="tabs d-flex">
+                                        <p onClick={() => setLocation(0)} className={`${index===0 && 'active'}`}>MOON</p>
+                                        <p onClick={() => setLocation(1)} className={`${index===1 && 'active'}`}>MARS</p>
+                                        <p onClick={() => setLocation(2)} className={`${index===2 && 'active'}`}>EUROPA</p>
+                                        <p onClick={() => setLocation(3)} className={`${index===3 && 'active'}`}>TITAN</p>
+                                    </div>
                                 </Col>
-                                <Col xs={12} sm={6} lg={7} xl={8}>
-                                    <Row>
-                                        <Col>
-                                            <p className="travel-text">EST. TRAVEL TIME</p>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <p className="travel">{destinations[0].travel.toUpperCase()}</p>
-                                        </Col>
-                                    </Row>
+                            </Row>
+                            <Row>
+                                <Col xs={12} lg={12} className="mt-3">
+                                    <h1>{item.name.toUpperCase()}</h1>
                                 </Col>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
+                            </Row>
+                            <Row>
+                                <Col xs={9} md={8} lg={8} xxl={6}>
+                                    <p className="description">{item.description}</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={9} md={8} lg={9} xxl={6} className="distance-travel d-flex align-items-end mt-4 mt-xxl-5">
+                                    <Col xs={12} sm={6} lg={7} xl={8}>
+                                        <Row>
+                                            <Col>
+                                                <p className="distance-text">AVG. DISTANCE</p>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <p className="distance">{item.distance.toUpperCase()}</p>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    <Col xs={12} sm={6} lg={7} xl={8}>
+                                        <Row>
+                                            <Col>
+                                                <p className="travel-text">EST. TRAVEL TIME</p>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <p className="travel">{item.travel.toUpperCase()}</p>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                })}
             </Container>
         </main>
     );
