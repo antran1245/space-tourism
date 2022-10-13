6e916d27c6093e74a5d573d0f84e44783a3741e9
antran1245 <antran1245@gmail.com>
2022-10-13 14:50:12 -0700

    technology desktop view initial setup

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -11,34 +11,34 @@ export default function Technology() {
         <main id="technology">
             <Container className="content">
                 <Row>
-                    <Col>
+                    <Col xl={{offset: 1}}>
                         <p className="headText"><span>03</span> SPACE  LAUNCH 101</p>
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
+                <Row className="content-info">
+                    <Col lg={{offset: 0, span: 2}} xxl={{offset: 1, span: 2}} className="tabs">
                         <div>1</div>
                         <div>2</div>
                         <div>3</div>
                     </Col>
-                    <Col>
+                    <Col lg={4} xxl={{span: 5}} className="info">
                         <Row>
                             <Col>
-                                <p>THE TERMINOLOGY</p>
+                                <p className="title">THE TERMINOLOGY ...</p>
                             </Col>
                         </Row>
                         <Row>
                             <Col>
-                                <p>{technology[0].name.toUpperCase()}</p>
+                                <p className="heading">{technology[0].name.toUpperCase()}</p>
                             </Col>
                         </Row>
                         <Row>
-                            <Col>
-                                <p>{technology[0].description}</p>
+                            <Col xxl={8}>
+                                <p className="description">{technology[0].description}</p>
                             </Col>
                         </Row>
                     </Col>
-                    <Col>
+                    <Col lg={6} xxl={4} className="p-0 d-flex justify-content-end">
                         <img src={require("../assets/technology"+portrait[0])} alt={technology[0].name}/>
                     </Col>
                 </Row>
