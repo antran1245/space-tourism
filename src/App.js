e8fe759c04125c8a981be3566875befe6c736935
antran1245 <antran1245@gmail.com>
2022-12-21 20:29:26 -0800

    browserRouter to  HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
 import Destination from './components/Destination';
@@ -8,15 +8,16 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navigation/>
+    <HashRouter basename='/'>
+      <Navigation />
       <Routes>
-        <Route path='space-tourism/' element={<Home/>}/>
-        <Route path='space-tourism/destination' element={<Destination/>}/>
-        <Route path='space-tourism/crew' element={<Crew/>}/>
-        <Route path='space-tourism/technology'  element={<Technology />}/>
+        <Route path='/space-tourism/' element={<Home />} />
+        <Route path='/space-tourism/destination' element={<Destination />} />
+        <Route path='/space-tourism/crew' element={<Crew />} />
+        <Route path='/space-tourism/technology' element={<Technology />} />
+        <Route path="*" element={<Navigate to={'/space-tourism/'} replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
